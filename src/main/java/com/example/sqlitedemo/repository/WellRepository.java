8a3bad2e9de1ac5ae595a355daad20ac022e1a24
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-21 13:58:21 +0500

    create interface WellRepository

---
+++ src/main/java/com/example/sqlitedemo/repository/WellRepository.java
@@ -0,0 +1,13 @@
+package com.example.sqlitedemo.repository;
+
+
+import com.example.sqlitedemo.entity.Well;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface WellRepository extends JpaRepository<Well, Long> {
+    @Query("SELECT e FROM Well e WHERE e.name = :name")
+    public Well getWellByWellname(@Param("name") String name);
+}
+
