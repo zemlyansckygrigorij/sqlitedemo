3fa97726bd381322bf7d61883a7201b74fa110ca
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-21 13:58:38 +0500

    create class Well

---
+++ src/main/java/com/example/sqlitedemo/entity/Well.java
@@ -0,0 +1,37 @@
+package com.example.sqlitedemo.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Well {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name="name",nullable = false, unique=true)
+    private String name;
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Equipment> comments = new ArrayList<>();
+    public Well() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
