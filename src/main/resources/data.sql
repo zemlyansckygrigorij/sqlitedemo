a799ccf37befde80a888cc9b01a4ead15bb2e35f
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-21 13:53:05 +0500

    create file data.sql for create table and insert values

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
-INSERT INTO PERSON(name,message) values("Varun","Hey this is Varun");
-INSERT INTO PERSON(name,message) values("Joe","Hey this is Joe");
+
+INSERT INTO WELL(name) values("Varun0");
+INSERT INTO WELL(name) values("Varun1");
+INSERT INTO WELL(name) values("Varun2");
+
+
+INSERT INTO EQUIPMENT(name) values("Varun0");
+INSERT INTO EQUIPMENT(name) values("Varun1");
+INSERT INTO EQUIPMENT(name) values("Varun2");
