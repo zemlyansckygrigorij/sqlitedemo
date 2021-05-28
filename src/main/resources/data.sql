82da6640e7c20eccb2986c0181792ac1cfedaec1
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-28 14:36:04 +0500

    update file data.sql adding insert into tables EQUIPMENT, WELL_EQUIPMENTS

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,15 @@
-
+DELETE FROM WELL;
+DELETE FROM EQUIPMENT;
+DELETE FROM WELL_EQUIPMENTS;
 INSERT INTO WELL(name) values("Varun0");
 INSERT INTO WELL(name) values("Varun1");
 INSERT INTO WELL(name) values("Varun2");
 
+INSERT INTO EQUIPMENT(name) values("eq0");
+INSERT INTO EQUIPMENT(name) values("eq1");
+INSERT INTO EQUIPMENT(name) values("eq2");
 
-INSERT INTO EQUIPMENT(name) values("Varun0");
-INSERT INTO EQUIPMENT(name) values("Varun1");
-INSERT INTO EQUIPMENT(name) values("Varun2");
+INSERT INTO WELL_EQUIPMENTS(well_id,equipments_id) values(1,1);
+INSERT INTO WELL_EQUIPMENTS(well_id,equipments_id) values(2,2);
+INSERT INTO WELL_EQUIPMENTS(well_id,equipments_id) values(3,3);
+
