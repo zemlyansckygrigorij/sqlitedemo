0548a959ee7aba1f21363a7f9de63b371883f688
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-28 14:37:14 +0500

    update interface EquipmentRepository

--- src/main/java/com/example/sqlitedemo/repository/EquipmentRepository.java
+++ src/main/java/com/example/sqlitedemo/repository/EquipmentRepository.java
@@ -4,9 +4,10 @@ import com.example.sqlitedemo.entity.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
     @Query("SELECT e FROM Equipment e WHERE e.name = :name")
     public Equipment getEquipmentByEquipmentname(@Param("name") String name);
-
 }
