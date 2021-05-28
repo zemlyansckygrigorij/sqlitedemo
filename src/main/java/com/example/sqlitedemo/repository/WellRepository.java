5647903706893caf99e431ac0a40a75111a8d56b
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-28 14:39:36 +0500

    update interface WellRepository

--- src/main/java/com/example/sqlitedemo/repository/WellRepository.java
+++ src/main/java/com/example/sqlitedemo/repository/WellRepository.java
@@ -5,7 +5,9 @@ import com.example.sqlitedemo.entity.Well;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WellRepository extends JpaRepository<Well, Long> {
     @Query("SELECT e FROM Well e WHERE e.name = :name")
     public Well getWellByWellname(@Param("name") String name);
