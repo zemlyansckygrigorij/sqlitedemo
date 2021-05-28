ea3376da02e4e3b49c3e11c6941b6c7a51607b42
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-28 14:41:41 +0500

    create class Message

---
+++ src/main/java/com/example/sqlitedemo/entity/Message.java
@@ -0,0 +1,25 @@
+package com.example.sqlitedemo.entity;
+
+public class Message {
+    private int number;
+    private String name;
+
+    public Message() {
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
