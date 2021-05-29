a539b47dee05955ea2f564783c261ee5d01fb002
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-05-29 13:31:41 +0500

    update class WellControllerTest

--- src/test/java/com/example/sqlitedemo/controller/WellControllerTest.java
+++ src/test/java/com/example/sqlitedemo/controller/WellControllerTest.java
@@ -1,7 +1,23 @@
 package com.example.sqlitedemo.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest
+@AutoConfigureMockMvc
 class WellControllerTest {
-
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private WellController controller;
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
 }
